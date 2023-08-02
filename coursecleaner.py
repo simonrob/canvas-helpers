0488a51f2e6e44854de867285c1d89842563d154
Simon Robinson <simon@robinson.ac>
2023-08-02 15:36:09 +0100

    Add navigation and settings reset option

--- coursecleaner.py
+++ coursecleaner.py
@@ -21,6 +21,8 @@ def get_args():
     parser.add_argument('url', nargs=1, help='Please provide the URL of the course to be cleaned')
     parser.add_argument('--all', action='store_true', help='Delete/reset all of a course\'s content (equivalent to '
                                                            'passing every other available option)')
+    parser.add_argument('--reset', action='store_true', help='Reset a course\'s navigation items to the default set, '
+                                                             'and update other general appearance and access settings')
     parser.add_argument('--pages', action='store_true',
                         help='Delete all of a course\'s pages (including the front page)')
     parser.add_argument('--modules', action='store_true', help='Delete all of a course\'s modules')
@@ -50,10 +52,10 @@ print('\nWARNING: this script allows you to delete *all* content from a Canvas c
       'create a backup at %s/content_exports first' % COURSE_URL.replace('api/v1/', ''))
 
 
-def confirm_deletion(type_hint):
+def confirm_action(type_hint, action_hint='deleting ALL'):
     print()
-    if input('Confirm deleting ALL %s for course "%s: %s"? (type yes or no) ' % (
-            type_hint, COURSE_CODE, COURSE_NAME)).lower() != 'yes':
+    if input('Confirm %s %s for course "%s: %s"? (type yes or no) ' % (
+            action_hint, type_hint, COURSE_CODE, COURSE_NAME)).lower() != 'yes':
         sys.exit('ERROR: aborting deletion; confirmation refused')
 
 
@@ -77,8 +79,91 @@ def delete_items(content_list_path, type_hint, params=None):
     print('Deleted', len(content_list_json), type_hint, 'items')
 
 
+if args.reset or args.all:
+    confirm_action(action_hint='resetting to the default all', type_hint='settings')
+
+    # reset navigation items
+    course_content_path = '%s/tabs' % COURSE_URL
+    course_content_response = Utils.canvas_multi_page_request(course_content_path, type_hint='course navigation list')
+    if not course_content_response:
+        print('ERROR: unable to retrieve course navigation list; aborting')
+        sys.exit()
+    course_content_json = json.loads(course_content_response)
+
+    ignored_tabs = ['home', 'settings']  # these items cannot be modified
+    tab_order = {
+        2: 'modules',  # positions are 1-based, but the first item is 'home'
+        3: 'assignments',
+        4: 'grades',
+        5: 'context_external_tool_46',  # reading list
+        6: 'discussions',
+        7: 'context_external_tool_95',  # panopto video
+    }
+
+    for item in course_content_json:
+        item_id = item['id']
+        if item_id in ignored_tabs:
+            continue
+
+        item_position = -1
+        item_hidden = True
+        if item_id in tab_order.values():
+            for key, value in tab_order.items():
+                if item_id == value:
+                    item_position = key
+                    item_hidden = False
+                    break
+
+        item_params = {'hidden': item_hidden}
+        if item_position > 0:
+            item_params['position'] = item_position
+        tab_update_response = requests.put('%s/%s' % (course_content_path, item_id), params=item_params,
+                                           headers=Utils.canvas_api_headers())
+        if tab_update_response.status_code == 200:
+            print('\tUpdated navigation item', item['label'], '- hidden:', item_hidden,
+                  ('(position: %d)' % item_position) if item_position > 0 else '')
+        else:
+            print('\tWARNING: unable to update navigation item %s:' % item['label'], tab_update_response.text)
+
+    # reset default view to the Page (`wiki`) that is set as the front page
+    course_settings = {
+        # note: if the front page is not set this will fail, which is why it is separated from other settings
+        'course[default_view]': 'wiki'  # `wiki` is a Page; can also be `modules`, `assignments`, etc (see Courses API)
+    }
+    course_update_response = requests.put(COURSE_URL, params=course_settings, headers=Utils.canvas_api_headers())
+    if course_update_response.status_code == 200:
+        print('\nReset course homepage to default (`wiki`)')
+    else:
+        print('\nWARNING: unable to reset course homepage:', course_update_response.text)
+
+    # update other basic settings
+    course_settings = {
+        'course[course_format]': 'on_campus',  # this course is taught in-person
+        'course[remove_image]': True,  # clear the course image
+        'course[remove_banner_image]': True  # clear the course banner image
+    }
+    course_update_response = requests.put(COURSE_URL, params=course_settings, headers=Utils.canvas_api_headers())
+    if course_update_response.status_code == 200:
+        print('\nReset course basic settings: set format to `on_campus` and removed default images')
+    else:
+        print('\nWARNING: unable to update course basic settings:', course_update_response.text)
+
+    # update advanced settings
+    course_settings = {
+        'default_due_time': '11:00:00',  # default due time: 11am
+        'restrict_student_future_view': True,  # restrict viewing before its start date
+        'restrict_student_past_view': True,  # restrict viewing after its end date
+        'hide_sections_on_course_users_page': True  # sections are just used for enrolment; no need to be visible
+    }
+    course_update_response = requests.put('%s/settings' % COURSE_URL, params=course_settings,
+                                          headers=Utils.canvas_api_headers())
+    if course_update_response.status_code == 200:
+        print('\nReset course advanced settings: set default deadline to 11am and restricted viewing outside start/end')
+    else:
+        print('\nERROR: unable to update course advanced settings:', course_update_response.text)
+
 if args.pages or args.all:
-    confirm_deletion(type_hint='pages')
+    confirm_action(type_hint='pages')
 
     course_content_path = '%s/pages' % COURSE_URL
     course_content_response = Utils.canvas_multi_page_request(course_content_path, type_hint='course pages')
@@ -111,7 +196,7 @@ if args.pages or args.all:
     print('Deleted', len(course_content_json), 'pages')
 
 if args.modules or args.all:
-    confirm_deletion(type_hint='modules')
+    confirm_action(type_hint='modules')
 
     course_content_path = '%s/modules' % COURSE_URL
     course_content_response = Utils.canvas_multi_page_request(course_content_path, type_hint='course modules')
@@ -148,7 +233,7 @@ if args.modules or args.all:
     print('Deleted', len(course_content_json), 'modules')
 
 if args.assignments or args.all:
-    confirm_deletion(type_hint='assignments')
+    confirm_action(type_hint='assignments')
 
     # assignments are split into groups, but unlike modules their APIs are not linked
     delete_items(content_list_path='%s/assignments' % COURSE_URL, type_hint='assignment')
@@ -157,34 +242,34 @@ if args.assignments or args.all:
     delete_items(content_list_path='%s/assignment_groups' % COURSE_URL, type_hint='assignment group')
 
 if args.rubrics or args.all:
-    confirm_deletion(type_hint='rubrics')
+    confirm_action(type_hint='rubrics')
     delete_items(content_list_path='%s/rubrics' % COURSE_URL, type_hint='rubric')
 
 if args.quizzes or args.all:
-    confirm_deletion(type_hint='quizzes')
+    confirm_action(type_hint='quizzes')
     delete_items(content_list_path='%s/quizzes' % COURSE_URL, type_hint='quiz')
 
     # "New Quizzes" have a completely different API path (of course they do)
     delete_items(content_list_path='%s/quizzes' % COURSE_URL.replace('/api/v1', '/api/quiz/v1'), type_hint='new quiz')
 
 if args.discussions or args.all:
-    confirm_deletion(type_hint='discussions')
+    confirm_action(type_hint='discussions')
     delete_items(content_list_path='%s/discussion_topics' % COURSE_URL, type_hint='discussion')
 
 if args.announcements or args.all:
-    confirm_deletion(type_hint='announcements')
+    confirm_action(type_hint='announcements')
 
     # announcements are retrieved via the discussions API with a special parameter
     delete_items(content_list_path='%s/discussion_topics' % COURSE_URL, type_hint='announcement',
                  params={'only_announcements': True})
 
 if args.events or args.all:
-    confirm_deletion(type_hint='events')
+    confirm_action(type_hint='events')
     delete_items('%s/calendar_events' % COURSE_URL.split('/courses')[0], type_hint='event',
                  params={'all_events': True, 'context_codes[]': ['course_%d' % COURSE_ID]})
 
 if args.files or args.all:
-    confirm_deletion(type_hint='files')
+    confirm_action(type_hint='files')
 
     # first we delete all folders (forcing deletion of non-empty items and their content)
     course_content_path = '%s/folders' % COURSE_URL

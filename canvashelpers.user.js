a81013f1b8842a5dc373eefb02cad53ca8e13424
Simon Robinson <simon@robinson.ac>
2025-10-13 11:32:31 +0100

    Switch to `fetch` interception for better reliability

--- canvashelpers.user.js
+++ canvashelpers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Canvas Helpers
 // @namespace    https://github.com/simonrob/canvas-helpers
-// @version      2025-09-30
+// @version      2025-10-13
 // @updateURL    https://github.com/simonrob/canvas-helpers/raw/main/canvashelpers.user.js
 // @downloadURL  https://github.com/simonrob/canvas-helpers/raw/main/canvashelpers.user.js
 // @require      https://gist.githubusercontent.com/raw/51e2fe655d4d602744ca37fa124869bf/GM_addStyle.js
@@ -13,7 +13,7 @@
 // @match        https://canvas.swansea.ac.uk/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=instructure.com
 // @grant        none
-// @run-at       document-end
+// @run-at       document-start
 // ==/UserScript==
 /* global GM_addStyle, waitForKeyElements */
 
@@ -21,8 +21,8 @@
     'use strict';
 
     // Convenience function for consistency (and easier filtering) in logged messages
-    function logCHMessage(message) {
-        console.log('Canvas Helpers UserScript: ' + message);
+    function logCHMessage(...message) {
+        console.log('Canvas Helpers UserScript: ', ...message);
     }
 
     GM_addStyle(`
@@ -44,76 +44,82 @@
     }
 
     // add a new button to clear all task list items
-    waitForKeyElements('.todo-list-header', function (header) {
-        const headerWrapper = document.createElement('div');
-        headerWrapper.setAttribute('class', 'h2 shared-space');
-        headerWrapper.setAttribute('style', 'margin-top: 18px;');
-        const clearIcon = document.createElement('a');
-        clearIcon.setAttribute('class', 'events-list icon-trash standalone-icon');
-        clearIcon.setAttribute('style', 'float: right; font-size: 12px; font-size: 0.75rem; font-weight: normal;');
-        clearIcon.setAttribute('href', '#');
-        clearIcon.addEventListener('click', function () {
-            const todoList = document.querySelectorAll('button[title="Ignore until new submission"]');
-            [...todoList].forEach(button => {
-                button.click();
+    function addClearTaskListButton() {
+        waitForKeyElements('.todo-list-header', function (header) {
+            const headerWrapper = document.createElement('div');
+            headerWrapper.setAttribute('class', 'h2 shared-space');
+            headerWrapper.setAttribute('style', 'margin-top: 18px;');
+            const clearIcon = document.createElement('a');
+            clearIcon.setAttribute('class', 'events-list icon-trash standalone-icon');
+            clearIcon.setAttribute('style', 'float: right; font-size: 12px; font-size: 0.75rem; font-weight: normal;');
+            clearIcon.setAttribute('href', '#');
+            clearIcon.addEventListener('click', function () {
+                const todoList = document.querySelectorAll('button[title="Ignore until new submission"]');
+                [...todoList].forEach(button => {
+                    button.click();
+                });
+                return false;
             });
-            return false;
+            clearIcon.textContent = 'Clear all';
+            header.parentNode.insertBefore(headerWrapper, header);
+            headerWrapper.appendChild(header);
+            headerWrapper.appendChild(clearIcon);
         });
-        clearIcon.textContent = 'Clear all';
-        header.parentNode.insertBefore(headerWrapper, header);
-        headerWrapper.appendChild(header);
-        headerWrapper.appendChild(clearIcon);
-    });
+    }
 
     // for the Python-based New Quiz integrations, we need a separate API key - make retrieving this easier
-    waitForKeyElements('div[role="main"] > div', function (container) {
-        const headerContainer = container.querySelector('div[data-automation="sdk-grading"]');
-        if (headerContainer) {
-            const existingButton = headerContainer.querySelector('button:first-of-type');
-            const newButton = existingButton.cloneNode(true);
-            newButton.querySelector('span[class$="baseButton__iconWrapper"]').remove(); // any existing icon
-            newButton.querySelector('span[class$="baseButton__children"]').textContent = 'Display/copy New Quiz API token';
-            newButton.addEventListener('click', function () {
-                navigator.clipboard.writeText(window.sessionStorage.access_token).catch((e) => {
-                    console.log('Unable to copy to clipboard:', e);
+    function addNewQuizAPIKeyButton() {
+        waitForKeyElements('div[role="main"] > div', function (container) {
+            const headerContainer = container.querySelector('div[data-automation="sdk-grading"]');
+            if (headerContainer) {
+                const existingButton = headerContainer.querySelector('button:first-of-type');
+                const newButton = existingButton.cloneNode(true);
+                newButton.querySelector('span[class$="baseButton__iconWrapper"]').remove(); // any existing icon
+                newButton.querySelector('span[class$="baseButton__children"]').textContent = 'Display/copy New Quiz API token';
+                newButton.addEventListener('click', function () {
+                    navigator.clipboard.writeText(window.sessionStorage.access_token).catch((e) => {
+                        logCHMessage('Unable to copy to clipboard:', e);
+                    });
+                    alert('Your current New Quiz API bearer token is:\n\n' + window.sessionStorage.access_token +
+                        '\n\nThis token has been copied to the clipboard to use as the `new_quiz_lti_bearer_token` value ' +
+                        'for Canvas Helpers New Quiz scripts.');
                 });
-                alert('Your current New Quiz API bearer token is:\n\n' + window.sessionStorage.access_token +
-                    '\n\nThis token has been copied to the clipboard to use as the `new_quiz_lti_bearer_token` value ' +
-                    'for Canvas Helpers New Quiz scripts.');
-            });
-            existingButton.after(newButton);
-        }
-    }, {waitOnce: false});
+                existingButton.after(newButton);
+            }
+        }, {waitOnce: false});
+    }
 
     // for the Python-based Studio integrations, we need a separate domain and API key - make retrieving this easier
-    waitForKeyElements('span[dir="ltr"],.StandaloneApp,#settings_page_wrapper', function (container) {
-        const menuContainer = container.querySelector('ul.NavTrayContent__navLinks,div.DefaultLayout__main');
-        if (menuContainer) {
-            const settingsButton = menuContainer.querySelector('li.NavTrayContent__navLinks-item:last-of-type,div[id^="tab-"]:last-of-type');
-            if (settingsButton && !menuContainer.querySelector('#canvas-helpers-studio-key-button')) {
-                const sessionToken = 'user_id="' + window.sessionStorage.userId + '", token="' + window.sessionStorage.token + '"';
-                const newButton = settingsButton.cloneNode(true);
-                newButton.id = 'canvas-helpers-studio-key-button';
-                const sidebarButton = newButton.querySelector('span[class$="truncateText"] > span');
-                const textLabel = 'Display/copy Studio API token';
-                if (sidebarButton) { // we match both the sidebar (which isn't always present) and the main settings page
-                    sidebarButton.style.marginTop = '12px';
-                    sidebarButton.textContent = textLabel;
-                } else {
-                    newButton.textContent = textLabel;
-                }
-                newButton.addEventListener('click', function () {
-                    navigator.clipboard.writeText(sessionToken).catch((e) => {
-                        console.log('Unable to copy to clipboard:', e);
+    function addStudioAPIKeyButton() {
+        waitForKeyElements('span[dir="ltr"],.StandaloneApp,#settings_page_wrapper', function (container) {
+            const menuContainer = container.querySelector('ul.NavTrayContent__navLinks,div.DefaultLayout__main');
+            if (menuContainer) {
+                const settingsButton = menuContainer.querySelector('li.NavTrayContent__navLinks-item:last-of-type,div[id^="tab-"]:last-of-type');
+                if (settingsButton && !menuContainer.querySelector('#canvas-helpers-studio-key-button')) {
+                    const sessionToken = 'user_id="' + window.sessionStorage.userId + '", token="' + window.sessionStorage.token + '"';
+                    const newButton = settingsButton.cloneNode(true);
+                    newButton.id = 'canvas-helpers-studio-key-button';
+                    const sidebarButton = newButton.querySelector('span[class$="truncateText"] > span');
+                    const textLabel = 'Display/copy Studio API token';
+                    if (sidebarButton) { // we match both the sidebar (which isn't always present) and the main settings page
+                        sidebarButton.style.marginTop = '12px';
+                        sidebarButton.textContent = textLabel;
+                    } else {
+                        newButton.textContent = textLabel;
+                    }
+                    newButton.addEventListener('click', function () {
+                        navigator.clipboard.writeText(sessionToken).catch((e) => {
+                            logCHMessage('Unable to copy to clipboard:', e);
+                        });
+                        alert('Your current Studio API bearer token is:\n\n' + sessionToken + '\n\nThis token has been ' +
+                            'copied to the clipboard to use as the `studio_lti_bearer_token` value for Canvas Helpers ' +
+                            'Studio scripts. Your `studio_lti_subdomain` value is:\n\n' + window.location.hostname);
                     });
-                    alert('Your current Studio API bearer token is:\n\n' + sessionToken + '\n\nThis token has been ' +
-                        'copied to the clipboard to use as the `studio_lti_bearer_token` value for Canvas Helpers ' +
-                        'Studio scripts. Your `studio_lti_subdomain` value is:\n\n' + window.location.hostname);
-                });
-                settingsButton.after(newButton);
+                    settingsButton.after(newButton);
+                }
             }
-        }
-    }, {waitOnce: false});
+        }, {waitOnce: false});
+    }
 
     // -----------------------------------------------------------------------------------------------------------------
     // Homepage: make course cards smaller and hide the "Published Courses" header (in staff view)
@@ -152,54 +158,45 @@
         `);
     }
 
+    // intercept dashboard card loading response to add our own items - see: https://stackoverflow.com/a/64961272
     const custom_data_scope = '/dashboard/custom_cards';
     const custom_data_url = '/api/v1/users/self/custom_data' + custom_data_scope;
     const custom_data_namespace = 'canvas-helpers';
 
-    // intercept dashboard card loading response to add our own items - see: https://stackoverflow.com/a/43144531
-    const open_prototype = XMLHttpRequest.prototype.open,
-        intercept_response = function (urlpattern, callback) {
-            XMLHttpRequest.prototype.open = function () {
-                arguments['1'].match(urlpattern) && this.addEventListener('readystatechange', function (event) {
-                    if (this.readyState === 4) {
-                        const response = callback(event.target.responseText);
-                        Object.defineProperty(this, 'response', {writable: true});
-                        Object.defineProperty(this, 'responseText', {writable: true});
-                        this.response = this.responseText = response;
-                    }
-                });
-                return open_prototype.apply(this, arguments);
-            };
-        };
+    const {fetch: origFetch} = window;
+    window.fetch = async (...args) => {
+        if (args[0].match(/dashboard_cards\?observed_user_id/i)) {
+            logCHMessage('Overriding dashboard card fetch to add custom cards');
+            try {
+                const originalCardsResponse = await origFetch(...args);
+                if (!originalCardsResponse.ok) {
+                    throw new Error('Error loading original dasboard cards: HTTP status:', originalCardsResponse.status);
+                }
+                const originalCards = await originalCardsResponse.json();
 
-    intercept_response(/dashboard_cards\?observed_user_id/i, function (response) {
-        try {
-            const json = JSON.parse(response);
+                const customFavouritesResponse = await origFetch(custom_data_url + '?ns=' + encodeURIComponent(custom_data_namespace));
+                if (!customFavouritesResponse.ok) {
+                    throw new Error('Error loading custom favourites: HTTP status:', customFavouritesResponse.status);
+                }
+                const customFavourites = await customFavouritesResponse.json();
 
-            // we need to use XHR for a synchronous request here as fetch is async but we need to insert mid-request
-            let customFavourites = [];
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', custom_data_url + '?ns=' + encodeURIComponent(custom_data_namespace), false); // false = synchronous
-            xhr.send();
-            if (xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                customFavourites = JSON.parse(data.data);
+                originalCards.push(...JSON.parse(customFavourites.data));  // custom content is JSON within JSON
+                return new Response(JSON.stringify(originalCards));
+            } catch (e) {
+                logCHMessage('CUSTOM Unable to parse dashboard card JSON', e);
+                return response;
             }
-
-            json.push(...customFavourites);
-            return JSON.stringify(json);
-        } catch (e) {
-            console.log('Unable to parse dashboard card JSON', e);
-            return response;
+        } else {
+            return await origFetch(...args);
         }
-    });
+    };
 
 
     // temporary method for adding a custom dashboard card manually - useful for courses that cannot be favourited
     // to use this, open the browser console and call addCustomCard(), then enter the JSON for the new card
     // minimal example custom card JSON:
     // {"shortName": "CS Final Year Projects", "courseCode": "CSP300/CSP301/CSP302/CSP344/CSP354", "href": "/courses/60749", "image": "[URL removed for brevity]", "published": true}
-    window.addCustomCard = function () {
+    unsafeWindow.addCustomCard = function () {
         fetch(custom_data_url + '?ns=canvas-helpers').then(r => r.json()).then(d => {
             let arr = JSON.parse(d.data || '[]');
             const new_card = prompt('Paste your new dashboard item as JSON (see this script\'s source for an example).\n\nEnter "CLEAR" to remove all custom cards.\n\nForce-refresh the page to apply your changes:');
@@ -256,5 +253,25 @@
             adminButton.href = '/accounts';
             searchCoursesButton.parentNode.appendChild(adminButton);
         }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // To intercept dashboard card requests, this script now runs at document-start.
+    // Because of this, we need to wait for the page body to load before using waitForKeyElements.
+    // -----------------------------------------------------------------------------------------------------------------
+    if (document.body) {
+        addClearTaskListButton();
+        addNewQuizAPIKeyButton();
+        addStudioAPIKeyButton();
+    } else {
+        const observer = new MutationObserver(function () {
+            if (document.body) {
+                observer.disconnect();
+                addClearTaskListButton();
+                addNewQuizAPIKeyButton();
+                addStudioAPIKeyButton();
+            }
+        });
+        observer.observe(document.documentElement, {childList: true});
     }
 })();
